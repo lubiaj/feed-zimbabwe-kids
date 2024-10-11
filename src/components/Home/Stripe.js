11f2b0ea52d35479546591df3884c9eda34f2466
Sarwar A Kawsar <sakawsar70@gmail.com>
2024-10-11 14:35:47 +0600

    Added the monthly tuition

--- src/components/Home/Stripe.js
+++ src/components/Home/Stripe.js
@@ -2,6 +2,22 @@ import Swal from 'sweetalert2'
 import Stripe from 'stripe';
 const stripe = new Stripe(process.env.REACT_APP_stripe_sk);
 const popup = () => {
+    let payment_links = {
+      one_time: {
+        2500 : 'https://donate.stripe.com/dR65lL9ln1qbdTq5ks',
+        1000 : 'https://donate.stripe.com/00g7tTbtv4Cn6qYfZ5',
+        250 : 'https://donate.stripe.com/5kA29z8hj6Kv8z6fZ4',
+        75 : 'https://donate.stripe.com/28o4hH2WZ5Gr16EaEJ',
+        25 : 'https://donate.stripe.com/14kbK9cxzecXcPmeUY'
+      },
+      monthly: {
+        2500 : "https://buy.stripe.com/3csdSh5574CncPm9AK",
+        1000 : "https://buy.stripe.com/00geWlgNP5GrbLieV5",
+        250 : "https://buy.stripe.com/4gwaG58hj2uf9DaaEQ",
+        75 : "https://buy.stripe.com/fZeaG5btvgl502AcMZ",
+        25 : "https://buy.stripe.com/3cs29zeFHecX16EfZc"
+      }
+    }
     Swal.fire({
         title: 'Giving To FEED Zimbabwe Kids',
         html:   "<div class=''>" +
@@ -23,14 +39,19 @@ const popup = () => {
                         "<input class='is-subscription' type='checkbox' id='is-subscription' checked/> I want to Give this amount every month." +
                       "</label>" +
                     "</div>" +
-                    "<select style='padding:16px;margin-bottom:16px;width:100%;' id='select_package'>" +
-                      "<option value='' selected disabled>Please select a donation</option>" +
-                      "<option value='https://donate.stripe.com/dR65lL9ln1qbdTq5ks'>$2,500 - Sponsor tuition for 100 children a semester</option>" +
-                      "<option value='https://donate.stripe.com/00g7tTbtv4Cn6qYfZ5'>$1,000 - Towards the building fund</option>" +
-                      "<option value='https://donate.stripe.com/5kA29z8hj6Kv8z6fZ4'>$250 - Sponsor tuition for 10 children a semester</option>" +
-                      "<option value='https://donate.stripe.com/28o4hH2WZ5Gr16EaEJ'>$75 - Sponsor a child tuition for a year</option>" +
-                      "<option value='https://donate.stripe.com/14kbK9cxzecXcPmeUY'>$25 - Sponsor a child tuition for a semester</option>" +
-                    "</select>" +
+                    "<div id='select_package'>" +
+                      "<select style='padding:16px;margin-bottom:16px;width:100%;' id='select_package_field'>" +
+                        "<option value='' selected disabled>Please select a donation</option>" +
+                        "<option value='2500'>$2,500 - Sponsor tuition for 100 children a semester</option>" +
+                        "<option value='1000'>$1,000 - Towards the building fund</option>" +
+                        "<option value='250'>$250 - Sponsor tuition for 10 children a semester</option>" +
+                        "<option value='75'>$75 - Sponsor a child tuition for a year</option>" +
+                        "<option value='25'>$25 - Sponsor a child tuition for a semester</option>" +
+                      "</select>" +
+                      "<label style='display: block; text-align: left;'>" +
+                        "<input class='tuition-is-subscription' type='checkbox' id='tuition-is-subscription' checked/> I want to Give this tuition amount every month." +
+                      "</label>" +
+                    "</div>" +
                 "</div>",
         allowOutsideClick: false,
         showCancelButton: true,
@@ -66,10 +87,17 @@ const popup = () => {
       preConfirm: (value) => {
           console.log(value)
           const amount = document.getElementById('amount').value;
-          const selectedPackage = document.getElementById('select_package').value;
+          const selectedPackage = document.getElementById('select_package_field').value;
+          let tuition_mode = 'one_time';
+          if( document.getElementById('package_amount').checked ) {
+            if( document.getElementById('tuition-is-subscription').checked ) {
+              tuition_mode = 'monthly';
+            }
+          }
+          console.log(tuition_mode, selectedPackage, payment_links[tuition_mode][selectedPackage])
           let output = {
             amount: document.getElementById('any_amount').checked ? amount : null,
-            redirectUrl: document.getElementById('package_amount').checked ? selectedPackage : null,
+            redirectUrl: document.getElementById('package_amount').checked ? payment_links[tuition_mode][selectedPackage] : null,
             mode: document.getElementById('is-subscription').checked == true ? 'subscription' : 'payment'
           }
           if (document.getElementById('any_amount').checked && !amount) {
